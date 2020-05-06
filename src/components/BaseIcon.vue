6337d14d989065a8949e90127d785a2eefa88ef6
bbj14 <kazuya1425m@gmail.com>
2020-05-06 20:46:19 +0900

    Lesson7

---
+++ src/components/BaseIcon.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="icon-wrapper">
+    <users-icon class="icon" :size="size"></users-icon>
+  </div>
+</template>
+
+<script>
+import { UsersIcon } from 'vue-feather-icons'
+
+export default {
+  props: {
+    size: {
+      type: [Number, String],
+      default: '24'
+    }
+  },
+  components: {
+    UsersIcon
+  }
+}
+</script>
+
+<style scoped>
+.icon-wrapper {
+  display: inline-flex;
+  align-items: center;
+  color: rgba(0, 0, 0, 0.4);
+  font-size: 1rem;
+  font-weight: 600;
+}
+.icon {
+  stroke: currentColor;
+  stroke-width: 2;
+  stroke-linecap: round;
+  stroke-linejoin: round;
+  fill: none;
+  margin-right: 6px;
+}
+</style>
