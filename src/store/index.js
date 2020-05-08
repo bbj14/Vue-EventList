62459a4441de5b36aef11f8ee25768c301309509
bbj14 <kazuya1425m@gmail.com>
2020-05-08 16:05:41 +0900

    Lesson4

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import EventService from '@/services/EventService.js'
 
 Vue.use(Vuex)
 
@@ -29,21 +30,23 @@ export default new Vuex.Store({
       { id: 2, title: '...', organizer: '...' },
       { id: 3, title: '...', organizer: '...' },
       { id: 4, title: '...', organizer: '...' }
-    ]
+    ],
+    count: 0
   },
-  mutations: {},
-  actions: {},
+  mutations: {
+    ADD_EVENT(state, event) {
+      state.events.push(event)
+    }
+  },
+  actions: {
+    createEvent({ commit }, event) {
+      return EventService.postEvent(event).then(() => {
+        commit('ADD_EVENT', event)
+      })
+    }
+  },
   modules: {},
   getters: {
-    catLength: state => {
-      return state.categories.length
-    },
-    doneTodos: state => {
-      return state.todos.filter(todo => todo.done)
-    },
-    activeTodosCount: state => {
-      return state.todos.filter(todo => !todo.done).length
-    },
     getEventById: state => id => {
       return state.events.find(event => event.id === id)
     }
