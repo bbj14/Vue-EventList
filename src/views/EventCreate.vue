f42fb64519e993e2b0cff05e219c7fd17286a81c
bbj14 <kazuya1425m@gmail.com>
2020-05-08 14:38:27 +0900

    Mastering Vuex Lesson3

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,3 +1,25 @@
 <template>
-  <h1>Create Event</h1>
+  <div>
+    <h1>Create an Event, {{ user.name }}</h1>
+    <p>Created by {{ user.id }}</p>
+    <p>{{ getEventById(2) }}</p>
+
+    <p>There are {{ catLength }} categories</p>
+    <ul>
+      <li v-for="cat in categories" :key="cat">{{ cat }}</li>
+    </ul>
+  </div>
 </template>
+
+<script>
+import { mapState, mapGetters } from 'vuex'
+
+export default {
+  computed: {
+    ...mapGetters(['catLength', 'getEventById']),
+    ...mapState(['user', 'categories'])
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
